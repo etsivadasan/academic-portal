4fd880cf8ca7d89fb5241aa47bac91628b04e4e0
Dr Sivadasan E T <etsivadasan@gmail.com>
2026-06-08 16:09:53 +0530

    port

--- app.py
+++ app.py
@@ -154,5 +154,7 @@ def add_reply():
     return jsonify({"success": True})
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    import os
+    # Tell Flask to listen on port 10000 (Render default) and open to the internet (0.0.0.0)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
